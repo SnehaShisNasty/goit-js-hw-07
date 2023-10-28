0e89b8132335ab3c2b54d706f776e8a48a068e11
SnehaShisNasty <katirina18@gmail.com>
2023-10-28 22:59:09 +0100

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+let instance = 1 + 2;
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      (item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="#">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="large-image.jpg"
+            alt="${item.description}"
+            />
+        </a>
+    </li>
+  `
+    )
+    .join("");
+}
+list.addEventListener("click", handleClick);
+function handleClick(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  function openModal() {
+    if (event.target === event.target) {
+      const findItem = galleryItems.find((item) => {
+        return item.preview === event.target.src;
+      });
+      instance = basicLightbox.create(`
+    <div class = "modal">
+        <img class="gallery__image" src="${findItem.original}">
+    
+    </div>`);
+
+      instance.show();
+    }
+  }
+  openModal();
+}
+function closeModalEsp(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+}
+
+document.addEventListener("keydown", closeModalEsp);
