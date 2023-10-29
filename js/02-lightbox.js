1d580f3b1cd9973b536392a4d02e64132ef5a138
SnehaShisNasty <katirina18@gmail.com>
2023-10-29 21:17:22 +0000

    add one more task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      (item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}"  data-title="123" ">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="large-image.jpg"
+            alt="${item.description}"
+            />
+        </a>
+    </li>
+  `
+    )
+    .join("");
+}
+const lightbox = new SimpleLightbox(".gallery a");
+
+document.querySelector(".gallery").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    lightbox.open();
+    setTimeout(() => {
+      //Я не розумію як достучатися до caption
+    }, 250);
+  }
+});
